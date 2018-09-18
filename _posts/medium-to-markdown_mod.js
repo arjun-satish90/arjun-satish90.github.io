ae11ec06e92578a090dd8dad3b72aa47610817aa
Will Koehrsen <wjk68@case.edu>
2018-09-18 17:56:59 -0400

    Writing a Blog Post article

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/parallelizing-feature-engineering-with-dask-3db88aec33b7").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-most-important-part-of-a-data-science-project-is-writing-a-blog-post-50715f37833a").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
