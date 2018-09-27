af6a81157c0a01394c022271ed47396ae07355e0
Will Koehrsen <wjk68@case.edu>
2018-09-27 15:59:16 -0400

    Added Featuretools on Spark

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/converting-medium-posts-to-markdown-for-your-blog-5d6830408467").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/feature-labs-engineering/featuretools-on-spark-e5aa67eaf807").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
