0a4164e5302e45e61d6bd4044c03ea4998b5bedc
Will K <wjk68@case.edu>
2018-10-14 09:25:54 -0400

    Added post on Simpson's Paradox

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/building-a-recommendation-system-using-neural-network-embeddings-1ef92e5c80c9").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/simpsons-paradox-how-to-prove-two-opposite-arguments-using-one-dataset-1c9c917f5ff9").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
