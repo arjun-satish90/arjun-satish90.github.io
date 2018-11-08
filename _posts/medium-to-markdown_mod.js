836c15bec29b9e80138cf179f4da087de8e48c1e
Will K <wjk68@case.edu>
2018-11-07 21:24:17 -0600

    The power of I don't know

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/simpsons-paradox-how-to-prove-two-opposite-arguments-using-one-dataset-1c9c917f5ff9").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-power-of-i-dont-know-590ab40d1995").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
