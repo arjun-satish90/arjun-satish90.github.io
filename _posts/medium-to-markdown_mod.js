6d8974668aef39a132939f31cdb81dba5e134e4b
Will K <wjk68@case.edu>
2018-11-07 21:25:03 -0600

    My weaknesses as a data scientist

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-power-of-i-dont-know-590ab40d1995").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/my-weaknesses-as-a-data-scientist-1310dab9f566").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
