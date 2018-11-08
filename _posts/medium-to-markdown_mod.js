734e01021227290dc1ccdf564a75063f6076a017
Will K <wjk68@case.edu>
2018-11-07 21:26:27 -0600

    Recurrent neural networks by example

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/biases-and-how-to-overcome-them-692c8c35f4a5").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/recurrent-neural-networks-by-example-in-python-ffd204f99470").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
