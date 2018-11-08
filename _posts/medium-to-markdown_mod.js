457a1134a36c125f71bacf8391be07249ead2f7d
Will K <wjk68@case.edu>
2018-11-07 21:25:46 -0600

    Biases and How to Overcome Them

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/my-weaknesses-as-a-data-scientist-1310dab9f566").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/biases-and-how-to-overcome-them-692c8c35f4a5").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
