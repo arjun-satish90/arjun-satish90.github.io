31356ac2d9623ee1f05414ec66bb9b3416e9de79
Will K <wjk68@case.edu>
2018-11-07 21:27:49 -0600

    Prediction Engineering

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/how-to-create-value-with-machine-learning-eb09585b332e").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/prediction-engineering-how-to-set-up-your-machine-learning-problem-b3b8f622683b").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
