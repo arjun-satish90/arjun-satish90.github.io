0fe3b72b784f93846c00f162959605d4be29a86d
Will K <wjk68@case.edu>
2018-11-22 15:45:53 -0600

    Added feature engineering post

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/prediction-engineering-how-to-set-up-your-machine-learning-problem-b3b8f622683b").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/feature-engineering-what-powers-machine-learning-93ab191bcc2d").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
