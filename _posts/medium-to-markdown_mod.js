4698891fb4b77523e1a88751a7c02cbacdce232f
Will K <wjk68@case.edu>
2018-11-22 15:46:30 -0600

    Added Modeling for Machine Learning Post

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/feature-engineering-what-powers-machine-learning-93ab191bcc2d").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/modeling-teaching-a-machine-learning-algorithm-to-deliver-business-value-ad0205ca4c86").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
