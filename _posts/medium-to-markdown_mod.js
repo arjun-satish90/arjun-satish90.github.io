fa5c12d69585d96a45d9e9a64f9b513129aba72f
Will K <wjk68@case.edu>
2018-11-22 15:47:47 -0600

    Added deploying on AWS

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/deploying-a-keras-deep-learning-model-as-a-web-application-in-p-fc0f2354a7ff").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/deploying-a-python-web-app-on-aws-57ed772b2319").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
