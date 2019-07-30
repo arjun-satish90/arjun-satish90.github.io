3ef7f54cda48af8038eb7214e5180fb5463adb68
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:37:11 -0400

    Added Real Life Superpowers

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/correlation-vs-causation-a-real-world-example-9e939c85581e").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/real-life-superpowers-c69d66b51ed4").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
