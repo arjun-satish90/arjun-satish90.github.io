5b61b40cb6f664d775db9b569d4a84ef4bd2a7e8
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:24:28 -0400

    Added Data Science: A Practical Application

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/improving-random-forest-in-python-part-1-893916666cd").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/data-science-a-practical-application-7056ec22d004").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
