e4922175e0d74660892bec73575fd6f2842ee610
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:06:44 -0400

    Data Visualization with Bokeh Part One

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/controlling-the-web-with-python-6fceb22c5f08").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/data-visualization-with-bokeh-in-python-part-one-getting-started-a11655a467d4").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
