b617b0a3b99c188b56e2aba56409ca4789005d69
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:46:19 -0400

    Added Slow Tech

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/markov-chain-monte-carlo-in-python-44f7e609be98").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/slow-tech-take-back-your-mind-5d142dc3ccb9").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
