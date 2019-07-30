9f0cee03bb31cce484e1fd654963e5b85dd9277b
WillKoehrsen <wjk68@case.edu>
2019-07-30 12:43:27 -0400

    Added Regretting the Present and Repo2Docker

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/introduction-to-interactive-time-series-visualizations-with-plotly-in-python-d3219eb7a7af").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/docker-without-the-hassle-b98447caedd8").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
