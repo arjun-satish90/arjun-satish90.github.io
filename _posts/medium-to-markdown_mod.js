e37292592377f60f57620b24f76e6f1460a1d09f
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:39:06 -0400

    Added Overfitting vs Underfitting

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/learn-by-sharing-4461cc93f8c1").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/overfitting-vs-underfitting-a-conceptual-explanation-d94ee20ca7f9").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
