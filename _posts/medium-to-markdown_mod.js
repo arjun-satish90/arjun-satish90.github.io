c3fd54ba645879775e26e441a340f0db80e74e21
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:20:53 -0400

    Added Automated Feature Engineering in Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/machine-learning-kaggle-competition-part-one-getting-started-32fb9ff47426").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/automated-feature-engineering-in-python-99baf11cc219").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
