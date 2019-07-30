2acad65fda626ba3cb8fb957133b57c2872102c2
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:22:40 -0400

    Added A Feature Selection Tool for Machine Learning in Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/machine-learning-kaggle-competition-part-two-improving-e5b4d61ab4b8").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/a-feature-selection-tool-for-machine-learning-in-python-b64dd23710f0").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
