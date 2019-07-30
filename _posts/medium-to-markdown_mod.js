4e94a3a52cfcc61ba26feb5e7b15d7befde0b821
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:45:35 -0400

    Added Markov Chain Monte Carlo in Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-multiple-comparisons-problem-e5573e8b9578").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/markov-chain-monte-carlo-in-python-44f7e609be98").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
