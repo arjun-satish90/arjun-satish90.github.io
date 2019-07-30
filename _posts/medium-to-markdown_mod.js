7eb21d8e231bab7214706650238d9228529b5bb9
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:47:15 -0400

    Added Bayes Rule Applied

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/slow-tech-take-back-your-mind-5d142dc3ccb9").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/bayes-rule-applied-75965e4482ff").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
