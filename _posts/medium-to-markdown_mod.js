fff558b15e4fdda8ec4f5112c89d73175f625338
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:33:49 -0400

    Added Thoughts on the Two Cultures

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/how-to-avoid-common-difficulties-in-your-data-science-programming-environment-1b78af2977df").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/thoughts-on-the-two-cultures-of-statistical-modeling-72d75a9e06c2").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
