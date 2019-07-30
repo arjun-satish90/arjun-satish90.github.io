bd795eb0934d262e715258905846fe7886e66f2f
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:09:00 -0400

    Added Data Analysis with Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-technology-frontier-db4dd061ea97").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/data-analysis-with-python-19434f5d6324").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
