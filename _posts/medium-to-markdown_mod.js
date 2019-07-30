e9f22a26f1703286040fa6357da3e66b3f2864f0
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:21:31 -0400

    Added How to Automatically Import

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/predicting-the-frequency-of-asteroid-impacts-with-a-poisson-processes-98d483efa61d").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/how-to-automatically-import-your-favorite-libraries-into-ipython-or-a-jupyter-notebook-9c69d89aa343").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
