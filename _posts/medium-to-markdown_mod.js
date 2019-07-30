548efb7c33e1dc761ab652b25127b9c68a3613d0
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:32:49 -0400

    Added How to Avoid Common

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/data-scientists-your-variable-names-are-awful-heres-how-to-fix-them-89053d2855be").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/how-to-avoid-common-difficulties-in-your-data-science-programming-environment-1b78af2977df").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
