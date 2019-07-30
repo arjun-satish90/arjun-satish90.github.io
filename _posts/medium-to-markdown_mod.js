4971f999d5662c71a28f70953acac148861c2bc2
WillKoehrsen <wjk68@case.edu>
2019-07-30 16:30:32 -0400

    Added Annoucing the Reality Project

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/analyzing-medium-story-stats-with-python-24c6491a8ff0").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/announcing-the-reality-project-e16cc71abb64").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
