a0bb9543fdec373d747c0b10a2ca47751758e0dc
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:08:08 -0400

    Added The Technology Frontier

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/exploratory-data-analysis-with-r-no-code-4c70de64c855").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-technology-frontier-db4dd061ea97").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
