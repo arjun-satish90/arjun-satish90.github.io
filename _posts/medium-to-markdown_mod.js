ce76230c4119266a8f2e80e6ccdab531bdce5a79
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:34:56 -0400

    Added Stock Prediction in Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-perils-of-rare-events-27dd1ae4e35e").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/stock-prediction-in-python-b66555171a2").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
