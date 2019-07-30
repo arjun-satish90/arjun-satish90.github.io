1679aa9902eed55e8c00254ad3d94ad8109d12fa
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:37:12 -0400

    Added The Poisson Distribution and Poisson Process Explained

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/lessons-from-how-to-lie-with-statistics-57060c0d2f19").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-poisson-distribution-and-poisson-process-explained-4e2cb17d459").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
