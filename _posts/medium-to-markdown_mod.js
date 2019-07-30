1604f9d10b930fe3b94b14ab3c9ba3746a3251c3
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:25:34 -0400

    Added 100 Miles Through the Park

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/set-your-jupyter-notebook-up-right-with-this-extension-24921838a332").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/100-miles-through-the-park-what-its-like-to-run-a-100-mile-ultramarathon-1ab36c46d76d").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
