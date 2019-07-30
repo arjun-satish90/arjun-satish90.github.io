968faea00342853c7c35f288d7b9ca56da351a04
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:12:01 -0400

    Added Controlling Your Location in Google Chrome

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/data-wrangling-with-python-and-sqlite-900d21bc5a53").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/controlling-your-location-in-google-chrome-6c0b216d1ba1").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
