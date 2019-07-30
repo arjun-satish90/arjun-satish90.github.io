f1bf2337483d5a1c5795c1b59e9b5fcad82b52b9
WillKoehrsen <wjk68@case.edu>
2019-07-30 16:44:51 -0400

    Added The Reality of Global Nuclear Weapons

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/a-non-technical-reading-list-for-data-science-d72451429a70").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-reality-of-global-nuclear-weapons-and-how-russian-nukes-turned-on-your-lights-6d55e056b516").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
