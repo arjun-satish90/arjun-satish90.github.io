03d519ed2265c995e8d65ba531f346042bb8a6a2
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:44:36 -0400

    Added The Multiple Comparisons Problem

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/python-is-the-perfect-tool-for-any-problem-f2ba42889a85").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-multiple-comparisons-problem-e5573e8b9578").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
