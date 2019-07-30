c4692cad8ec75e3c734e935c7601c40f44de1a7f
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:18:36 -0400

    Added Random Forest in Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-case-for-criticism-964c43f7e058").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/random-forest-in-python-24d0893d51c0").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
