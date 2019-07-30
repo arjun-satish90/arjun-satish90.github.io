564a45c6e9be5cfefe91050758cb53b9e0926c39
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:25:30 -0400

    Added Automated Machine Learning

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/an-introductory-example-of-bayesian-optimization-in-python-with-hyperopt-aae40fff4ff0").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/automated-machine-learning-hyperparameter-tuning-in-python-dfda59b72f8a").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
