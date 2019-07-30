ee80228f778f1f474f550bf7f71e1fa6c93a58e6
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:20:07 -0400

    Added Machine Learning Kaggle Part One

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/automated-machine-learning-on-the-cloud-in-python-47cf568859f").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/machine-learning-kaggle-competition-part-one-getting-started-32fb9ff47426").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
