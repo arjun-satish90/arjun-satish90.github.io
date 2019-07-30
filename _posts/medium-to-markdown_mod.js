bd74521d6053650fafa13ca0768a714073f96da5
WillKoehrsen <wjk68@case.edu>
2019-07-30 16:46:25 -0400

    Added The Myth of Us Vs Them

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-reality-of-global-nuclear-weapons-and-how-russian-nukes-turned-on-your-lights-6d55e056b516").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-myth-of-us-vs-them-e0bfccb62f41").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
