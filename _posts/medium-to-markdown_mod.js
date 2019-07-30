579666fbc7e63dd44cdd22591b058f39e358f26d
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:12:17 -0400

    Added Bayesian Linear Regression Part One

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/introduction-to-bayesian-linear-regression-e66e60791ea7").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/bayesian-linear-regression-in-python-using-machine-learning-to-predict-student-grades-part-1-7d0ad817fca5").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
