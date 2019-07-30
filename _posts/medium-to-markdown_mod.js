7e0d82b9b78db854ce66954cd237a756557bfbb4
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:08:35 -0400

    Added Histograms and Density Plots in Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/data-visualization-with-bokeh-in-python-part-ii-interactions-a4cf994e2512").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/histograms-and-density-plots-in-python-f6bda88f5ac0").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
