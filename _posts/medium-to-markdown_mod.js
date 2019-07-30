880e288a5a94dd5b589c37beb2e02179b6f68048
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:33:00 -0400

    Added The Perils of Rare Events

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/a-review-of-the-coursera-machine-learning-specialization-b889f5793648").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-perils-of-rare-events-27dd1ae4e35e").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
