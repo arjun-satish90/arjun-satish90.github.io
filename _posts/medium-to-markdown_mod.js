11befa9a85f0f21caca7337257c4baf23a651a2d
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:04:08 -0400

    Added Beyond Accuracy

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/unintended-consequences-and-goodharts-law-68d60a94705c").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/beyond-accuracy-precision-and-recall-3da06bea9f6c").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
