f40d7f0f8ebfe659cdc44ff12d94accd2352cc90
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:24:40 -0400

    Added An Introduction Example of Bayesian Optimization

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/a-conceptual-explanation-of-bayesian-model-based-hyperparameter-optimization-for-machine-learning-b8172278050f").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/an-introductory-example-of-bayesian-optimization-in-python-with-hyperopt-aae40fff4ff0").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
