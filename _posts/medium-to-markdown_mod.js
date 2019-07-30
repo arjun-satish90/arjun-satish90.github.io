da7c42f6646a7586d3ee1c280b9f764cd500f860
WillKoehrsen <wjk68@case.edu>
2019-07-30 12:52:02 -0400

    Added Copernican Principle

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/books-of-2018-f51600aa922a").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-copernican-principle-and-how-to-use-statistics-to-figure-out-how-long-anything-will-last-9cceb7aba20a").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
