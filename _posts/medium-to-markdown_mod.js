94537c014838505d2201f2c848ebd31228f6265a
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:50:18 -0400

    Added Screw the Environment but Consider Your Wallet

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-poisson-distribution-and-poisson-process-explained-4e2cb17d459").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/screw-the-environment-but-consider-your-wallet-a4f7cd3d3161").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
