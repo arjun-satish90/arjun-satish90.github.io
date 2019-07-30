c3e2b254e3ab9e300792cf95511e0de7e355b544
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:26:26 -0400

    Added A Theory of Prediction

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-simple-science-of-global-warming-5a2070bcd03a").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/a-theory-of-prediction-10cb335cc3f2").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
