4e6cb4084ff773e753887e42db52f1349fcdf772
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:57:39 -0400

    Added Unintended Consequences and Goodhart's Law

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/data-visualization-hackathon-style-c6dcaabbf626").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/unintended-consequences-and-goodharts-law-68d60a94705c").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
