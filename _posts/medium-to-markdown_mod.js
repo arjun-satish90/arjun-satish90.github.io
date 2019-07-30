31897657b9da276037f1dd08f417db4e39721ef1
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:28:59 -0400

    Added Time Series Analysis in Python

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/hyperparameter-tuning-the-random-forest-in-python-using-scikit-learn-28d2aa77dd74").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/time-series-analysis-in-python-an-introduction-70d5a5b1d52a").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
