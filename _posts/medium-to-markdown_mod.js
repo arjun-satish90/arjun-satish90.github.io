4644744029fa4deabb645d0e3f4161bb80a7903a
WillKoehrsen <wjk68@case.edu>
2019-07-30 12:56:24 -0400

    Added Analyzing Medium Story Stats

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/the-copernican-principle-and-how-to-use-statistics-to-figure-out-how-long-anything-will-last-9cceb7aba20a").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/analyzing-medium-story-stats-with-python-24c6491a8ff0").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
