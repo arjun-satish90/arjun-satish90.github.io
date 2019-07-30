513b14412e5ebeb0957e76a6f45b82716fd81c8d
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:24:33 -0400

    Added Set Your Notebook Up Right

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/a-data-science-public-service-announcement-fe81fbe6dc3f").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/set-your-jupyter-notebook-up-right-with-this-extension-24921838a332").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
