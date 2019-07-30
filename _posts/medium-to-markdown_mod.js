357a90df7c33d67518a919bb3ca3c370457d1907
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:32:00 -0400

    Added A Review of the Coursera Machine Learning Specialization

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/stock-analysis-in-python-a0054e2c1a4c").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/a-review-of-the-coursera-machine-learning-specialization-b889f5793648").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
