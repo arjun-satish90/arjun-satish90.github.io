d1d7b4dd437989aaf5d308a182d089e6d52e92d7
WillKoehrsen <wjk68@case.edu>
2019-07-30 18:06:12 -0400

    Added Exploratory Data Analysis with R

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/facial-recognition-using-googles-convolutional-neural-network-5aa752b4240e").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/exploratory-data-analysis-with-r-f9d3a4eb6b16").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
