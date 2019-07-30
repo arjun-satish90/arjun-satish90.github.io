c6bd8977df05ba529663ad0359b048e3cdc1ddad
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:26:28 -0400

    Added Machine Learning Kaggle Part Three

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/automated-machine-learning-hyperparameter-tuning-in-python-dfda59b72f8a").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/machine-learning-kaggle-competition-part-three-optimization-db04ea415507").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
