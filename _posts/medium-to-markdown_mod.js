a24c538e9e9c98d295a2e1cf18c50a318a30d442
WillKoehrsen <wjk68@case.edu>
2019-07-30 12:46:59 -0400

    Added articles On Blame and What I Learned

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/docker-without-the-hassle-b98447caedd8").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/what-i-learned-in-2018-30e2b5481c01").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
