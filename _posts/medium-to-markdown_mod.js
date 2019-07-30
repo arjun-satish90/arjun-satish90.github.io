f9e1c19b10f23340655166e959e1f56ac9101542
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:11:26 -0400

    Added Introduction to Bayesian Linear Regression

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/visualizing-data-with-pair-plots-in-python-f228cf529166").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/introduction-to-bayesian-linear-regression-e66e60791ea7").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
