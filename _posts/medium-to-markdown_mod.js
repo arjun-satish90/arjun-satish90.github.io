a17559e427b968a3b47f0315b7c5e852c3a04a12
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:23:29 -0400

    Added A Data Science Public Service Announcement

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/how-to-automatically-import-your-favorite-libraries-into-ipython-or-a-jupyter-notebook-9c69d89aa343").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/a-data-science-public-service-announcement-fe81fbe6dc3f").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
