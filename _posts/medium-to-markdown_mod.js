3eb12e2672144ae2d58c21e5f75477c708457e87
WillKoehrsen <wjk68@case.edu>
2019-07-30 17:52:42 -0400

    Added Capstone Project

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/the-vanquishing-of-war-plague-and-famine-b424ac967a41").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/capstone-project-mercedes-benz-greener-manufacturing-competition-4798153e2476").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
