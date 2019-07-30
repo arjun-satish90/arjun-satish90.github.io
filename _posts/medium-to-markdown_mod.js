04a15cf9d098d8f3c9988b5d8e66d2545cbb02e9
WillKoehrsen <wjk68@case.edu>
2019-07-30 19:27:21 -0400

    Added How to Get the Right Data

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/machine-learning-kaggle-competition-part-three-optimization-db04ea415507").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/how-to-get-the-right-data-why-not-ask-for-it-d26ced1bbd46").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
