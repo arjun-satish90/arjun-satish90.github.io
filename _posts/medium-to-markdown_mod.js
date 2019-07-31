54212cd924ffd72b0c0b6e3cc5dc1261c663528a
WillKoehrsen <wjk68@case.edu>
2019-07-31 14:34:44 -0400

    Added How to Access Pages Missing

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://towardsdatascience.com/how-to-get-the-right-data-why-not-ask-for-it-d26ced1bbd46").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/how-to-access-pages-missing-from-the-internet-ba8a399e9739").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
