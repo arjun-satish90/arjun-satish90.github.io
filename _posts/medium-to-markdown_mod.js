c47d99cdb461d96c80914ee50b4e18184b686f2a
WillKoehrsen <wjk68@case.edu>
2019-08-04 17:08:04 -0400

    Added Most People Screw Up Multiple Percentage Changes

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
  
-mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/how-to-access-pages-missing-from-the-internet-ba8a399e9739").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@williamkoehrsen/most-people-screw-up-multiple-percent-changes-heres-how-to-do-get-them-right-b86bd6ef4b72").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
